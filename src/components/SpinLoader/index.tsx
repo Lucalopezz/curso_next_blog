778ac3ba62cf27d8351fbbf8530e08adb9594c63
Lucas <lucaslops12@gmail.com>
2025-12-18 18:05:00 -0300

    feat: implement post listing with suspense

--- src/components/SpinLoader/index.tsx
+++ src/components/SpinLoader/index.tsx
@@ -1,10 +1,10 @@
 type SpinLoaderProps = {
-  containerClasses?: string;
+  className?: string;
 };
 
-export function SpinLoader({ containerClasses = "" }: SpinLoaderProps) {
+export function SpinLoader({ className = "" }: SpinLoaderProps) {
   return (
-    <div className={`flex items-center justify-center ${containerClasses}`}>
+    <div className={`flex items-center justify-center ${className}`}>
       <div
         className="w-10 h-10 border-5 border-t-transparent
        border-slate-900 rounded-full animate-spin"

8e3a036a766b7fefc927fa728c4b61f47da422d4
Lucas <lucaslops12@gmail.com>
2025-12-19 17:07:45 -0300

    feat: add PostHeading component and integrate it into HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
+import { PostHeading } from "@/components/PostHeading";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
+import Image from "next/image";
+import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function HomePage() {
@@ -9,6 +12,38 @@ export default async function HomePage() {
   return (
     <Container>
       <Header />
+
+      <section
+        className="grid grid-cols-1 gap-8 mb-16
+        sm:grid-cols-2 
+        group"
+      >
+        <Link href="#" className="w-full h-full overflow-hidden rounded-xl">
+          <Image
+            className="w-full h-full object-cover object-center group-hover:scale-105 transition"
+            src="/images/bryen_0.png"
+            width={1200}
+            height={720}
+            alt="Título do post"
+            // because it's in the first fold of the page, priority is needed (other images can use lazy loading)
+            priority
+          />
+        </Link>
+        <div className="flex flex-col gap-1 sm:justify-center">
+          <time className="text-slate-600 text-sm/tight" dateTime="2025-4-20">
+            20/04/2025
+          </time>
+          <PostHeading url="#" as="h1">Ola</PostHeading>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+            delectus vero nihil rem, at commodi facilis necessitatibus animi
+            officiis eos! Fugit odit, perferendis reiciendis distinctio ut
+            placeat porro veritatis? Natus.
+          </p>
+        </div>
+      </section>
+
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>

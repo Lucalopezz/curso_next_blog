9d63e37c45f19fc8ea989f8eb14194556bfe6009
Lucas <lucaslops12@gmail.com>
2025-12-20 16:08:25 -0300

    feat: add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+export default function NotFoundPage() {
+  return (
+    <div
+      className="min-h-80 bg-slate-950 text-slate-100
+    mb-16 p-8 rounded-2xl
+    flex items-center justify-center text-center
+    "
+    >
+      <div>
+        <h1 className="text-7xl/tight mb-4 font-extrabold">404</h1>
+        <p>Erro 404 - Página não encontrada</p>
+      </div>
+    </div>
+  );
+}

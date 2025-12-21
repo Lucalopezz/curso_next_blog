ff6b97dbc47abc39f575ecae0cffa1f78c808f66
Lucas <lucaslops12@gmail.com>
2025-12-21 14:35:04 -0300

    feat: add ErrorMessage component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,11 @@
+import ErrorMessage from "@/components/ErrorMessage";
+
 export default function NotFoundPage() {
   return (
-    <>
-      <title>Página não encontrada</title>
-      <div
-        className="min-h-80 bg-slate-950 text-slate-100
-    mb-16 p-8 rounded-2xl
-    flex items-center justify-center text-center
-    "
-      >
-        <div>
-          <h1 className="text-7xl/tight mb-4 font-extrabold">404</h1>
-          <p>Erro 404 - Página não encontrada</p>
-        </div>
-      </div>
-    </>
+    <ErrorMessage
+      pageTitle="Página não encontrada"
+      contentTitle="404 😿"
+      content="Erro 404 - A página que você está tentando acessar não existe neste site."
+    />
   );
 }
---
+++ src/components/ErrorMessage/index.tsx
@@ -0,0 +1,28 @@
+type ErrorMessageProps = {
+  pageTitle: string;
+  contentTitle: string;
+  content: React.ReactNode;
+};
+
+export default function ErrorMessage({
+  pageTitle,
+  contentTitle,
+  content,
+}: ErrorMessageProps) {
+  return (
+    <>
+      <title>{pageTitle}</title>
+      <div
+        className="min-h-80 bg-slate-950 text-slate-100
+    mb-16 p-8 rounded-2xl
+    flex items-center justify-center text-center
+    "
+      >
+        <div>
+          <h1 className="text-7xl/tight mb-4 font-extrabold">{contentTitle}</h1>
+          <div>{content}</div>
+        </div>
+      </div>
+    </>
+  );
+}

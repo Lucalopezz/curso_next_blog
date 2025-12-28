b12de71e16b713e231af1e5088286200ed55a68e
Lucas <lucaslops12@gmail.com>
2025-12-28 10:37:57 -0300

    feat: implement AdminPostLayout and MenuAdmin components for admin post management

---
+++ src/components/admin/MenuAdmin/index.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { CircleXIcon, FileTextIcon, HouseIcon, MenuIcon, PlusIcon } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export function MenuAdmin() {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setIsOpen(false);
+  }, [pathname]);
+
+  const navClasses = `bg-slate-900 text-slate-100 rounded-lg flex flex-col mb-8 sm:flex-row sm:flex-wrap ${
+    !isOpen && "h-10 overflow-hidden"
+  } sm:overflow-visible sm:h-auto`;
+  // [&>svg]:w-[16px] [&>svg]:h-[16px] -> make to access svg inside link and set width and height
+  const linkClasses =
+    "[&>svg]:w-[16px] [&>svg]:h-[16px] px-4 flex items-center justify-start gap-2 transition hover:bg-slate-800 rounded-lg h-10 shrink-0 cursor-pointer";
+  const openCloseBtnClasses = linkClasses + "text-blue-200 italic sm:hidden";
+  return (
+    <nav className={navClasses}>
+      <button
+        onClick={() => setIsOpen((s) => !s)}
+        className={openCloseBtnClasses}
+      >
+        {!isOpen && (
+          <>
+            <MenuIcon />
+            Menu
+          </>
+        )}
+        {isOpen && (
+          <>
+            <CircleXIcon />
+            Fechar
+          </>
+        )}
+      </button>
+      <a className={linkClasses} href="/" target="_blank">
+        <HouseIcon />
+        Home
+      </a>
+      <Link className={linkClasses} href="/admin/post">
+        <FileTextIcon />
+        Posts
+      </Link>
+      <Link className={linkClasses} href="/admin/post/new">
+        <PlusIcon />
+        Criar Post
+      </Link>
+    </nav>
+  );
+}

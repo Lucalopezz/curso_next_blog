8fb3b2b6bfff51cce2e2bac4af5488b6b90d05d7
Lucas <lucaslops12@gmail.com>
2025-12-29 17:21:02 -0300

    feat: create MenagePostForm to include author, title, and cover image URL fields

--- src/components/admin/MenagePostForm/index.tsx
+++ src/components/admin/MenagePostForm/index.tsx
@@ -5,32 +5,74 @@ import { Input } from "@/components/Input";
 import { MarkdownEditor } from "@/components/MarkdownEditor";
 import { useState } from "react";
 import { ImageUploader } from "../ImageUploader";
+import { InputCheckbox } from "@/components/InputCheckbox";
 
 export function MenagePostForm() {
   const [contentValue, setContentValue] = useState("");
   return (
-    <form className="mb-16">
+    <form action="" className="mb-16">
       <div className="flex flex-col gap-6">
-        <Input labelText="Nome" placeholder="Digite seu nome" type="password" />
-
-        <Input labelText="Sobrenome" placeholder="Digite seu sobrenome" />
-
-        <ImageUploader />
+        <Input
+          labelText="ID"
+          name="id"
+          placeholder="ID gerado automaticamente"
+          type="text"
+          defaultValue={""}
+          readOnly
+        />
 
         <Input
-          disabled
-          labelText="Sobrenome"
-          placeholder="Digite seu sobrenome"
-          defaultValue="Olá mundo"
+          labelText="Slug"
+          name="slug"
+          placeholder="Slug gerada automaticamente"
+          type="text"
+          defaultValue={""}
+          readOnly
+        />
+
+        <Input
+          labelText="Autor"
+          name="author"
+          placeholder="Digite o nome do autor"
+          type="text"
+          defaultValue={""}
+        />
+
+        <Input
+          labelText="Título"
+          name="title"
+          placeholder="Digite o título"
+          type="text"
+          defaultValue={""}
+        />
+
+        <Input
+          labelText="Excerto"
+          name="excerpt"
+          placeholder="Digite o resumo"
+          type="text"
+          defaultValue={""}
         />
 
         <MarkdownEditor
           labelText="Conteúdo"
-          disabled={false}
-          textAreaName="content"
           value={contentValue}
           setValue={setContentValue}
+          textAreaName="content"
+          disabled={false}
         />
+
+        <ImageUploader />
+
+        <Input
+          labelText="URL da imagem de capa"
+          name="coverImageUrl"
+          placeholder="Digite a url da imagem"
+          type="text"
+          defaultValue={""}
+        />
+
+        <InputCheckbox labelText="Publicar?" name="published" type="checkbox" />
 
         <div className="mt-4">
           <Button size="md" variant="default" type="submit">

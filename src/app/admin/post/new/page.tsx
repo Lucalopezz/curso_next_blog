a52d9c646bbfe5b605f0c238175c41d77b67f432
Lucas <lucaslops12@gmail.com>
2025-12-29 12:39:30 -0300

    feat: add Markdown editor and refactor post creation form

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,51 +1,10 @@
-import { Button } from "@/components/Button";
-import { Input } from "@/components/Input";
-import { InputCheckbox } from "@/components/InputCheckbox";
+import { MenagePostForm } from "@/components/admin/MenagePostForm";
+
 
 export const dynamic = "force-dynamic";
 
 export default async function AdminPostNewPage() {
   return (
-    <form action="" className="mb-16">
-      <div className="flex flex-col gap-6">
-        <Input labelText="Nome" placeholder="Digite seu nome" type="password" />
-
-        <Input labelText="Sobrenome" placeholder="Digite seu sobrenome" />
-
-        <InputCheckbox labelText="Sobrenome" />
-
-        <Input
-          disabled
-          labelText="Sobrenome"
-          placeholder="Digite seu sobrenome"
-          defaultValue="Olá mundo"
-        />
-
-        <Input
-          disabled
-          labelText="Sobrenome"
-          placeholder="Digite seu sobrenome"
-        />
-
-        <Input
-          labelText="Sobrenome"
-          placeholder="Digite seu sobrenome"
-          readOnly
-        />
-
-        <Input
-          labelText="Sobrenome"
-          placeholder="Digite seu sobrenome"
-          defaultValue="Olá mundo"
-          readOnly
-        />
-
-        <div className="mt-4">
-          <Button size="md" variant="default" type="submit">
-            Enviar
-          </Button>
-        </div>
-      </div>
-    </form>
+    <MenagePostForm/>
   );
 }
---
+++ src/components/admin/MenagePostForm/index.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+import { InputCheckbox } from "@/components/InputCheckbox";
+import { MarkdownEditor } from "@/components/MarkdownEditor";
+import { useState } from "react";
+
+export function MenagePostForm() {
+  const [contentValue, setContentValue] = useState("");
+  return (
+    <form className="mb-16">
+      <div className="flex flex-col gap-6">
+        <Input labelText="Nome" placeholder="Digite seu nome" type="password" />
+
+        <Input labelText="Sobrenome" placeholder="Digite seu sobrenome" />
+
+        <InputCheckbox labelText="Sobrenome" />
+
+        <Input
+          disabled
+          labelText="Sobrenome"
+          placeholder="Digite seu sobrenome"
+          defaultValue="Olá mundo"
+        />
+
+        <MarkdownEditor
+          labelText="Conteúdo"
+          disabled={false}
+          textAreaName="content"
+          value={contentValue}
+          setValue={setContentValue}
+        />
+
+        <div className="mt-4">
+          <Button size="md" variant="default" type="submit">
+            Enviar
+          </Button>
+        </div>
+      </div>
+    </form>
+  );
+}

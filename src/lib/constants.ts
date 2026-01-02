14ea1f3675ae87fce819f9a5892e2b901887a7cc
Lucas <lucaslops12@gmail.com>
2026-01-02 15:58:57 -0300

    feat: update image upload configuration and add example env file

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,6 @@
-export const IMAGE_UPLOAD_MAX_SIZE = 921600; // 900 * 1024 = 921600
-export const IMAGE_UPLOAD_DIRECTORY = 'uploads' 
-export const IMAGE_SERVER_URL = 'http://localhost:3000/uploads' 
+export const IMAGE_UPLOAD_MAX_SIZE =
+  Number(process.env.NEXT_PUBLIC_IMAGE_UPLOAD_MAX_SIZE) || 921600; // 900 * 1024 = 921600
+export const IMAGE_UPLOAD_DIRECTORY =
+  process.env.IMAGE_UPLOAD_DIRECTORY || "uploads";
+export const IMAGE_SERVER_URL =
+  process.env.IMAGE_SERVER_URL || "http://localhost:3000/uploads";

bf62d0c7d5d728e3eeca8959af5a1d6b545c8235
Lucas <lucaslops12@gmail.com>
2026-01-03 12:03:37 -0300

    feat: add metadata for login page and ensure login session in post layout

--- src/app/admin/post/layout.tsx
+++ src/app/admin/post/layout.tsx
@@ -1,10 +1,12 @@
 import { MenuAdmin } from "@/components/admin/MenuAdmin";
+import { requireLoginSessionOrRedirect } from "@/lib/login/manage-login";
 
-export default function AdminPostLayout({
+export default async function AdminPostLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  await requireLoginSessionOrRedirect();
   return (
     <>
       <MenuAdmin />
